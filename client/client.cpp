1284fc329370b5d6914cbb2c0263b03d610a9a63
Rich von Lehe <rhvonlehe@gmail.com>
2020-01-23 20:34:13 -0600

    Able to send the server some text successfully

--- client/client.cpp
+++ client/client.cpp
@@ -4,6 +4,55 @@
 
 using namespace boost;
 
+struct Session
+{
+    std::shared_ptr<asio::ip::tcp::socket> sock;
+    std::string buf;
+    std::size_t total_bytes_written;
+};
+
+void callback(const boost::system::error_code& ec,
+              std::size_t bytes_transferred,
+              std::shared_ptr<Session> s)
+{
+    if (ec.value() != 0)
+    {
+        std::cout << "Error occured! Error code = "
+                  << ec.value()
+                  << ". Message: " << ec.message();
+        return;
+    }
+    s->total_bytes_written += bytes_transferred;
+    if (s->total_bytes_written == s->buf.length()) {
+        return;
+    }
+    s->sock->async_write_some(
+                asio::buffer(
+                    s->buf.c_str() +
+                    s->total_bytes_written,
+                    s->buf.length() -
+                    s->total_bytes_written),
+                std::bind(callback, std::placeholders::_1,
+                          std::placeholders::_2, s));
+}
+
+void writeToSocket(std::shared_ptr<asio::ip::tcp::socket> sock)
+{
+    std::shared_ptr<Session> s(new Session);
+
+    s->buf = std::string("This is the text");
+    s->total_bytes_written = 0;
+    s->sock = sock;
+
+    sock->async_write_some(asio::buffer(s->buf),
+                           std::bind(callback,
+                                     std::placeholders::_1,
+                                     std::placeholders::_2,
+                                     s));
+
+
+}
+
 int main()
 {
 #if 0
@@ -28,16 +77,16 @@ int main()
     std::string raw_ip_address = "127.0.0.1";
     unsigned short port_num = 8888;
 
+    asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address),
+                               port_num);
+
+    asio::io_context ioc;
+
+    auto sock = std::make_shared<asio::ip::tcp::socket>(ioc, ep.protocol());
+
     try
     {
-        asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address),
-                                   port_num);
-
-        asio::io_context ioc;
-
-        asio::ip::tcp::socket sock(ioc, ep.protocol());
-
-        sock.connect(ep);
+        sock->connect(ep);
     }
     catch (system::system_error& e)
     {
@@ -46,5 +95,7 @@ int main()
         return e.code().value();
     }
 
+
+    writeToSocket(sock);
     return 0;
 }
